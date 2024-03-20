24106aaf21236e6377a6a3e3bd1f262401c0552f
tarunpakhare <72435351+tarunpakhare@users.noreply.github.com>
2024-03-20 16:28:45 +0530

    Update diameter.dart

--- diameter.dart
+++ diameter.dart
@@ -12,47 +12,40 @@ class diameter {
 
     for (var points in coordinates) {
       for (var point in points) {
-        double deltaX = point.dx - centroid[0];
-        double deltaY = point.dy - centroid[1];
+        double deltaX = point.dx - centroid[0]; //centering data
+        double deltaY = point.dy - centroid[1]; //centering data
         X.add(deltaX);
         Y.add(deltaY);
-        double squaredDistance = (deltaX * deltaX) + (deltaY * deltaY);
-        //double z = pow(point.dx - centroid[0], 2) +
-        //    pow(point.dy - centroid[1], 2).toDouble();
+        double squaredDistance = (deltaX * deltaX) + (deltaY * deltaY); //Z = X.*X + Y.*Y;
         Z.add(squaredDistance);
         zMean += squaredDistance;
         pointsCount++;
       }
     }
-    print("Z:$Z");
-    print("zMean=$zMean");
+  
 
-    zMean /= pointsCount; //(coordinates.length * coordinates[0].length);
+    zMean /= pointsCount; //Zmean = mean(Z);
     print("zMeanUpdated=$zMean");
     List<double> Z0 = [];
     for (var z in Z) {
       double z0 = (z - zMean) / (2 * sqrt(zMean));
       Z0.add(z0);
     }
-    print("Z0:$Z0");
+    print("Z0:$Z0"); //Z0 = (Z-Zmean)/(2*sqrt(Zmean));
 
-    /* List<List<double>> ZXY = [];
-    for (int i = 0; i < X.length; i++) {
-      ZXY.add([Z0[i].toDouble(), X[i].toDouble(), Y[i].toDouble()]);
-    }
-    Array2d abc = Array2d(ZXY);*/
+  
     Array zo = Array(Z0);
     Array x = Array(X);
     Array y = Array(Y);
     List<Array> zxy = Array2d([zo, x, y]);
-    print("ZXY:$zxy");
+    print("ZXY:$zxy"); //ZXY = [Z0 X Y];
 
     Array2d zxytranspose = matrixTranspose(Array2d([zo, x, y]));
     print("Transpose:$zxytranspose");
 
     Array2d svdResult = svd(zxytranspose);
-    print("svdResult:$svdResult");
-    print("Till here executed successfully");
+    print("svdResult:$svdResult"); //[U,S,V]=svd(ZXY,0);
+   
     Array2d svdResultTranspose = matrixTranspose(svdResult);
     print("Transposed SVDResult:$svdResultTranspose");
     for (var i = 0; i < svdResultTranspose.length; i++) {
@@ -61,21 +54,13 @@ class diameter {
       }
     }
     print('Changed signs: $svdResultTranspose');
-    List<double> A = getcolumn(svdResultTranspose, 2);
+    List<double> A = getcolumn(svdResultTranspose, 2); //A = V(:,3);
     print("First A:$A");
-    //List<double> A = svdResult[2].toList();
-    //print("A:$A");
-    A[0] = A[0] / (2 * sqrt(zMean));
+    A[0] = A[0] / (2 * sqrt(zMean)); //A(1) = A(1)/(2*sqrt(Zmean));
     print("Second A:$A");
-    A.add(-(zMean) * A[0]);
+    A.add(-(zMean) * A[0]); //A = [A ; -Zmean*A(1)];
     print("Third A:$A[0]");
-    /*double a = A[1];
-    double b = A[2];
-    List<double> center = [
-      -(a / (2 * A[0])) + centroid[0],
-      -(b / (2 * A[0])) + centroid[1]
-    ];
-    print("Center:$center");*/
+    
     double disc = ((A[1] * A[1]) + (A[2] * A[2]) - (4 * A[0] * A[3]));
     print(sqrt(disc.abs()));
     print((A[0].abs() / 2));
@@ -93,21 +78,9 @@ class diameter {
     double diameter = 2 * radius;
     print("Diameter:$diameter");
 
-    return diameter; //aise hi rakha hai
-    /* A[0] = A[0] / (2 * sqrt(zMean));
-    A.add(-zMean * A[0]);
-
-    double xCenter = -(A[1] + centroid[0] / 2 * A[0]);
-    double yCenter = -(A[2] + centroid[1] / 2 * A[0]);
-    double radius =
-        sqrt(pow(A[1], 2)) + pow(A[2], 2) - 4 * A[0] * A[3] / (2 * A[0]);
-    print(xCenter);
-    print(yCenter);
-    double d = 2 * radius;
-    print("Diameter=$d");
-    return 2 * radius;*/
+    return diameter; //Par = [-(A(2:3))'/A(1)/2+centroid , sqrt(A(2)*A(2)+A(3)*A(3)-4*A(1)*A(4))/abs(A(1))/2];
   }
-
+  //calculating the centroid
   List<double> calculateCentroid(List<List<Offset>> xy) {
     double sumX = 0.0;
     double sumY = 0.0;
@@ -123,15 +96,15 @@ class diameter {
     print("SumX:$sumX");
     print("SumY:$sumY");
     print("pointCount:$pointCount");
-    double centerX = sumX / pointCount; //(xy.length * xy[0].length);
-    double centerY = sumY / pointCount; //(xy.length * xy[0].length);
+    double centerX = sumX / pointCount; 
+    double centerY = sumY / pointCount; 
 
     print("centerX:$centerX");
     print("centerY:$centerY");
 
     return [centerX, centerY];
   }
-
+  //To get column
   List<double> getcolumn(List<List<double>> matrix, int col) {
     List<double> colValues = matrix.map((row) => row[col]).toList();
     print(colValues);
